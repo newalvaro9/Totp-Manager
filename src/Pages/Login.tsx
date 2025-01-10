2624fa3cf99e68ba6b9c172b6a2f36f1b0ee2616
newalvaro9 <alvaropobladoresteban9@gmail.com>
2025-01-10 21:52:02 +0100

    feat: clear password on send

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -14,6 +14,7 @@ export default function Login({ setShowPassword }: Props) {
     const [password, setPassword] = useState("");
 
     const handleSubmit = async () => {
+        setPassword("");
         try {
             if (showResetPrompt) {
                 await storage.resetStorage(password);
